650636256932176d7835f9693bc288fd33d2a673
madhuarukala <madhu.arukala5@gmail.com>
2017-09-21 08:45:22 +0530

    test

--- MavenDemo/src/com/Demo.java
+++ MavenDemo/src/com/Demo.java
@@ -1,5 +1,11 @@
 package com;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.annotation.Retention;
 
 import org.openqa.selenium.WebDriver;
@@ -22,13 +28,21 @@ public class Demo {
 		
 	}
 	@Test
-	public static void fun2(){
+	public static void fun2() throws IOException{
 		
 //		WebDriver driver=new FirefoxDriver();
 //		driver.get("http://google.com");
 //		driver.quit();
 		System.out.println("Test1");
 		
+		String cmd="";
+		Process p = Runtime.getRuntime().exec("cmd /c cd E: && cd E:/MyGIT/SimpleInterviewPrograms && git diff --name-only \"@{1 day ago}\" > t.txt ");
+		InputStream bis= p.getInputStream();
+		int i;
+		while((i=bis.read())!=-1)
+			System.out.print((char)i);
+		System.out.println();
+		System.out.println("****************************************************************");
 	}
 
 }
